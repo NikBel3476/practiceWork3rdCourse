991fdb29a084f6421c37d1522c4f1c0b0f2cdd0b
NikBel3476 <nikita.belkin3476@gmail.com>
2022-12-07 21:47:14 +0400

    initial commit

---
+++ practiceWork3rdCourse/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace practiceWork3rdCourse.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
